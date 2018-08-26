20849c2490e6da475de088786cc74a2f0fefc116
Julien Thieffry <julien.thieffry@eamslab.com>
2018-08-26 18:55:25 +0900

    launch.sh: main launcher for ND dset creation

---
+++ src/launch.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+
+  key="$1"
+
+  case $key in
+    -n|--number)
+      NUMBER="$2"
+      shift
+      shift
+      ;;
+    -s|--scale)
+      SCALE="$2"
+      shift
+      shift
+      ;;
+    -t|--threshold)
+      THRESHOLD="$2"
+      shift
+      shift
+      ;;
+    -f|--filename)
+      FILENAME="$2"
+      shift
+      shift
+      ;;
+    *)
+      POSITIONAL+=("$1")
+      shift
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL[@]}"
+
+source setup_env.sh
+
+python3 main.py -n $NUMBER -s $SCALE -t $THRESHOLD -f $FILENAME
