ae55f1a4f42b9499d56aac9b97200ec1a69cb598
Julien Thieffry <julien.thieffry@eamslab.com>
2018-08-31 17:10:01 +0900

    setup_env.sh: adding resize and class env

--- src/setup_env.sh
+++ src/setup_env.sh
@@ -1,5 +1,9 @@
 export ND_DSET_FOLDER=$(pwd)/../
 export ND_DSET_OBJ_NAME=nuts_
 export ND_DSET_BG_NAME=backgrd
+export ND_DSET_RESIZE_FLAG=1
+export ND_DSET_RESIZE_COL=416
+export ND_DSET_RESIZE_ROW=416
+export ND_DSET_CLASS=1
 
 
