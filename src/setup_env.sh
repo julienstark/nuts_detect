11e9dccec1749cae0b490135f3d12a25009996e4
Julien Thieffry <julien.thieffry@eamslab.com>
2018-08-31 18:27:32 +0900

    setup_env.sh: adding some info on the exports

--- src/setup_env.sh
+++ src/setup_env.sh
@@ -1,9 +1,16 @@
+# Main folder:
 export ND_DSET_FOLDER=$(pwd)/../
+# Prefix of the image name you would like to input:
 export ND_DSET_OBJ_NAME=nuts_
+# Prefix of the background images names:
 export ND_DSET_BG_NAME=backgrd
+# Put 1 if your want to enable img resizing, 0 otherwise:
 export ND_DSET_RESIZE_FLAG=1
+# If resizing is set, the new col number for the background image:
 export ND_DSET_RESIZE_COL=416
+# If resizing is set, the new row number for the background image:
 export ND_DSET_RESIZE_ROW=416
+# The class number needed for the detection algorithm:
 export ND_DSET_CLASS=1
 
 
