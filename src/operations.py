ca4640de6d36d62d7a8010e938d26496e1ce9e40
Julien Thieffry <julien.thieffry@eamslab.com>
2018-08-31 17:17:48 +0900

    operations.py: adding better resize function

--- src/operations.py
+++ src/operations.py
@@ -35,3 +35,21 @@ def resize_image(img, resize_fact):
     """
 
     return cv2.resize(img, resize_fact)
+
+def get_resize_prop(in_col, in_row, out_col, out_row):
+    """Get the resize proportions.
+
+    Args:
+        in_col: An integer representing the column in.
+        in_row: An integer representing the row in.
+        out_col: An integer representing the column out.
+        out_row: An integer representing the row out.
+
+    Returns:
+        A tuple representing the proportion between in and out.
+    """
+
+    col_prop = out_col / in_col
+    row_prop = out_row / in_row
+
+    return (col_prop, row_prop)
