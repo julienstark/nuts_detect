958342faf40df394d4b7902dde02e64e38434e5f
Julien Thieffry <julien.thieffry@eamslab.com>
2018-08-31 13:59:11 +0900

    operations.py: adding resize function

--- src/operations.py
+++ src/operations.py
@@ -7,6 +7,8 @@ function get_file_number: Get the number of files in a folder.
 
 import os
 
+import cv2
+
 def get_file_number(path):
     """Get file number for a folder.
 
@@ -16,5 +18,20 @@ def get_file_number(path):
     Returns:
         An int representing the number of files in a folder.
     """
+
     nbr = [n for n in os.listdir(path) if os.path.isfile(os.path.join(path, n))]
     return len(nbr)
+
+
+def resize_image(img, resize_fact):
+    """Resize an image with factor resize_fact.
+
+    Args:
+        img: A numpy array representing the image to resize.
+        resize_fact: A float representing the scaling factor.
+
+    Returns:
+        An numpy array representing the resized image.
+    """
+
+    return cv2.resize(img, resize_fact)
