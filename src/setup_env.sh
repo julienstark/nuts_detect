0a0503dbf30b89fa11fea3765a6b75354bd24610
Julien Thieffry <julien.thieffry@eamslab.com>
2018-12-04 14:28:04 +0900

    setup_env.sh: ROI should be in ENV

--- src/setup_env.sh
+++ src/setup_env.sh
@@ -4,6 +4,14 @@ export ND_DSET_FOLDER=$(pwd)/../
 export ND_DSET_OBJ_NAME=banana
 # Prefix of the background images names:
 export ND_DSET_BG_NAME=background
+# Column start pixel value for the background ROI:
+export ND_DSET_BG_ROI_CS=35
+# Row start pixel value for the background ROI:
+export ND_DSET_BG_ROI_RS=49
+# Column end pixel value for the background ROI:
+export ND_DSET_BG_ROI_CE=245
+# Row end pixel value for the background ROI:
+export ND_DSET_BG_ROI_RE=144
 # Put 1 if your want to enable img resizing, 0 otherwise:
 export ND_DSET_RESIZE_FLAG=1
 # If resizing is set, the new col number for the background image:
